fbfd01460283f699fe2496360757da98543090e3
CXCbI4 <aleksei6620@gmail.com>
2019-12-09 11:24:40 +0300

    Updates

---
+++ precache-manifest.31473d2f31d82d1493476f2c561ba5c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e82dc5a533499786ebfd4b1d95d96b4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b1bf92be8656022356ea",
+    "url": "./static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "0046338c1a9f4a5ff780",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "b1bf92be8656022356ea",
+    "url": "./static/js/2.feb90187.chunk.js"
+  },
+  {
+    "revision": "0046338c1a9f4a5ff780",
+    "url": "./static/js/main.ccc99e38.chunk.js"
+  },
+  {
+    "revision": "4af149cf989594b362c1",
+    "url": "./static/js/runtime-main.ebec13c3.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
